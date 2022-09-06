0db5935eadbf78743359ddbe67d02fdd35f71de7
MaximeDeconinck <maxime.deconinck@dauphine.eu>
2022-09-06 16:57:27 +0200

    TD1 : Question 1 to 4

---
+++ TD1/src/fr/dauphine/javaavance/td1/Exo1.java
@@ -0,0 +1,24 @@
+package fr.dauphine.javaavance.td1;
+
+public class Exo1 {
+	
+	public static void main(String[] args) {
+		
+		// 2 - Pressing Ctrl + Space when typing sysout automatically type this :
+		System.out.println();
+		
+	}
+	
+	// 3 - When typing toStr :
+	@Override
+	public String toString() {
+		TODO();
+		return super.toString();
+	}
+	
+	// 4 - When typing main : 
+	public static void main(String[] args) {
+		
+	}
+	
+}
