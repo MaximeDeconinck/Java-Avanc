c06a8cb2003008dced049eab16e92f289ac6eb6e
MaximeDeconinck <maxime.deconinck@dauphine.eu>
2022-09-25 16:33:52 +0200

    TD1.4 : Question 3 and 4

--- TD1/src/fr/dauphine/javaavance/td1/PolyLine.java
+++ TD1/src/fr/dauphine/javaavance/td1/PolyLine.java
@@ -1,5 +1,5 @@
 package fr.dauphine.javaavance.td1;
-import fr.dauphine.javaavance.td1.*;
+import fr.dauphine.javaavance.td1.Point;
 import java.util.ArrayList;
 
 public class PolyLine {
@@ -14,9 +14,28 @@ public class PolyLine {
 	}
 	
 	// 2 - Because we can add as many points as we want in a array list, we add a condition before the insertion
-	public void addPoint(Point p) {
+	public void add(Point p) {
 		if (!(this.list.size() == this.taille)) {
 			this.list.add(p);
 		}
+	}
+	
+	// 3 - 
+	public int pointCapacity() {
+		return this.taille;
+	}
+	
+	public int nbPoints() {
+		return this.list.size();
+	}
+	
+	// 4 -
+	public boolean contains(Point p1) {
+		for (Point p2 : this.list) {
+			if (Point.isSameAs(p1, p2) == true) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
